09c92041bea0d971e8283bde49688e11b930f7ba
priyadarshii786 <tathagatp918@gmail.com>
2023-05-31 00:06:04 +0530

    TC: O(n) using Two-Pointer method

--- 11(b). Container With Most Water.cpp
+++ 11(b). Container With Most Water.cpp
@@ -1,1 +1,29 @@
-.
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        // jai ganesh
+
+        // using Two-Pointer approach
+        //  TC: O(n)
+        //  SC: O(1)
+
+        int i = 0, j = height.size() - 1;
+        int max_capacity = INT_MIN;
+
+        while (i < j)
+        {
+            int water = (j - i) * min(height[i], height[j]);
+            max_capacity = max(max_capacity, water);
+
+            if (height[i] < height[j])
+                i++;
+
+            else
+                j--;
+        }
+
+        return max_capacity;
+    }
+};
