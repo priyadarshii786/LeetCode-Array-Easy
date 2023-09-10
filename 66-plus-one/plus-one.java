6a0810cec17a406c60d17b389bf7165f60d619cb
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2023-09-11 02:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (32.21%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        //jai ganesh
+        // https://youtu.be/iBk_JyxxuN8
+        
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+            
+        }
+        //------> below code is for the case when there is only single element in the array.
+        int[] newNumber=new int[n+1];
+        newNumber[0]=1;
+        return newNumber;
+    }
+}
